03f82a58273b480c97d6578c7aaac3cad799b493
jhunior cruz <jhuniorcruz@gmail.com>
2025-02-04 15:49:09 -0500

    TAREA 2

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
+    show BuildContext, MaterialApp, StatelessWidget, Widget;
 import 'package:numeros_whats_app/core/screens/detalles_numero_de_telefono.dart';
 import 'package:numeros_whats_app/core/screens/pagina_principal.dart';
+import 'package:numeros_whats_app/display/routes/routes.dart';
+import 'package:numeros_whats_app/display/screens/detalles_numero_de_telefono.dart';
+import 'package:numeros_whats_app/display/screens/pagina_principal.dart';
 import 'package:numeros_whats_app/routes/routes.dart';
 
 class MainApp extends StatelessWidget {
